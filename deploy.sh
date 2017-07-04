4393c82ba799f7f25a826b8dc80a1c676035436d
jeslopcru <jeslopcru@gmail.com>
2017-07-04 23:31:35 +0200

    initial commit hugo

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+hugo -t 
+rm -rf /tmp/ghpage-jesuslc-public
+cp -R public /tmp/ghpage-jesuslc-public
+rm -rf public
+git checkout -- .
+git checkout master
+rm -rf ./*
+cp -R /tmp/ghpage-jesuslc-public/* .
+git add --all
+git commit -m "New Deploy"
+git push origin master
+git checkout source
