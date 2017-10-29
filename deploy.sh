caeb296f6952a31f68b1037096c09da59fad39c2
jesuslc <jlopez@qashops.com>
2017-10-29 11:29:40 +0100

    some fixed

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 git submodule update --recursive --remote
-&& hugo
-&& rm -rf /tmp/ghpage-jesuslc-public
-&& cp -R public /tmp/ghpage-jesuslc-public
-&& rm -rf public
-&& git checkout -- .
-&& git checkout master
-&& rm -rf ./*
-&& cp -R /tmp/ghpage-jesuslc-public/* .
-&& git add --all
-&& git commit -m "New Deploy"
-&& git push origin master
-&& git checkout source
+hugo
+rm -rf /tmp/ghpage-jesuslc-public
+cp -R public /tmp/ghpage-jesuslc-public
+rm -rf public
+git checkout -- .
+git checkout master
+rm -rf ./*
+cp -R /tmp/ghpage-jesuslc-public/* .
+git add --all
+git commit -m "New Deploy"
+git push origin master
+git checkout source
